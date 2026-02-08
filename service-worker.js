1991cf8a81ad0ca9d86adbf80a893b155e8d0b91
raushansync <raushanguptaicloud@gmail.com>
2026-02-09 03:22:35 +0530

    lectures to video lessons, and link removed with aria disabled

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* RaushanSYNC Computer Science & Data Science PWA Service Worker */
 
-const CACHE_VERSION = 'csds-v1.0.2';
+const CACHE_VERSION = 'csds-v1.0.2.5';
 const CORE_CACHE = 'rs-core-' + CACHE_VERSION;
 const RUNTIME_CACHE = 'rs-runtime-' + CACHE_VERSION;
 
